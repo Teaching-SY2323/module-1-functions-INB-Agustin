599b625e965fed42fb9b3332a437dc0c5e0c6121
INB-Agustin <154307252+INB-Agustin@users.noreply.github.com>
2023-12-21 21:10:43 +0800

    Functions_AddValues.cpp

--- main.cpp
+++ main.cpp
@@ -2,27 +2,32 @@
 
 using namespace std;
 
-int Add2Values(int, int);
+int Add2Values(float, float);
 
 int main()
 {
-    int x, y;
+    float x, y;
     cout
         << "-- Add 2 Values --\n"
         << "Enter x Values: ";
     cin >> x;
-    cout << "Enter y Value: ";
-    cin >> y;
-
-    cout << "The value of the two numbers is: ";
-
-    cout << Add2Values(x, y);
-
+    if (x >= 'A' && x<= 'z'){
+    	cout << "Enter y Value: ";
+    	cin >> y;
+    	cout << "The value of the two numbers is: " << y ;
+	}else if (!(x >= 'A' && x<= 'z')) {
+		cout << "Enter y Value: ";
+		cin >> y;
+		cout << Add2Values(x, y);
+	}else {
+		cout << "Invalid! Please Enter a Valid Input";
+	}
+	
     return 0;
     
 }
 
-int Add2Values(int x, int y)
+int Add2Values(float x, float y)
 {
     // make your code here
     int total = x + y;
